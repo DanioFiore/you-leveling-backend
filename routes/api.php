0e52b91285c1694782e3ab110d4f6a2de09337e4
Danio <daniofioredev@gmail.com>
2025-03-29 21:33:26 +0100

    added register and login endpoint + auth middleware group

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestsController;
 
 // USE THIS ROUTE FOR TESTING
 Route::get('/test', [TestsController::class, 'test']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Apply sanctum authentication middleware to a group of routes
+Route::middleware('auth:sanctum')->group(function () {
+    
+});
